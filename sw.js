44f4039e12e9bdf2bb44449cead119be275839c7
Karol-OC <karol.oc2006@gmail.com>
2026-03-15 19:38:34 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "todo-pwa-v1"
+
+const urls = [
+  "index.html"
+]
+
+self.addEventListener("install", e => {
+
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(urls))
+  )
+
+})
+
+self.addEventListener("fetch", e => {
+
+  e.respondWith(
+    caches.match(e.request)
+    .then(res => res || fetch(e.request))
+  )
+
+})
